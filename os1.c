23bc5896899986e75c6131a1a138cbda6b9eec1d
maninder1046 <90817762+maninder1046@users.noreply.github.com>
2022-08-31 15:08:33 +0530

    Create os1.c

---
+++ os1.c
@@ -0,0 +1,18 @@
+//program to demonstrate the use of fork() system call
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+fork();
+fork();
+printf("%d \n",getpid());
+return 0;
+}#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+fork();
+fork();
+printf("%d \n",getpid());
+return 0;
+}
